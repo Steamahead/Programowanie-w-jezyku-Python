48ab4ca5eb1993e72c93bb85dc70c291baaa233c
Damian Sadza <sadza.damian@gmail.com>
2025-12-07 11:44:23 +0100

    zad_2b.j.py

---
+++ zad_2b.j.py
@@ -0,0 +1,5 @@
+x = [1, 2, 3, 4, 5]
+def grinder (number):
+    step_1 = [item * 2 for item in number]
+    return step_1
+print(grinder(x))
--- zad_2b.py
+++
@@ -1,5 +0,0 @@
-def my_function(data):
-    for numbers in data:
-        print(numbers * 2)
-var = [1, 2, 3, 4, 5]
-my_function(var)
