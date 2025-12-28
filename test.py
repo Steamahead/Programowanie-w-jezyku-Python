babbaf240141e41070b21d922748d70d6e69d700
Damian Sadza <sadza.damian@gmail.com>
2025-12-28 19:46:58 +0100

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-def calculate_discount(price: float, discount: float) -> float:
-    if not (0 <= discount <= 1):
-        raise ValueError("Zniżka musi być w zakresie od 0 do 100")
-
-    return price * (1 - discount)
